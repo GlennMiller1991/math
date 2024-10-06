1ff0584df507975b0f03571650bfe791bb6857f2
AlexB <gatesoftommorow91@mail.ru>
2024-10-06 10:27:40 +0400

    interpolation

---
+++ src/algorithm/line-interpolation.ts
@@ -0,0 +1,10 @@
+import type { INonEmptyArray } from "../type.utils.js";
+
+export class LineInterpolation {
+
+    
+    static interpolate(...controlPoints: INonEmptyArray<{controlPoint: number, value: number}>, atPoint: number) {
+        
+    }
+
+}
---
+++ src/type.utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Non empty array of T
+ */
+export type INonEmptyArray<T = any> = [T, ...T[]]
+
+
+type IFixedLengthArrayUtil<TLength extends number, TItem extends any, TArray extends Array<TItem>> =
+    TArray['length'] extends TLength ? TArray : IFixedLengthArrayUtil<TLength, TItem, [TItem, ...TArray]>
+
+/**
+ * Array of T of fixed length
+ */
+export type IFixedLengthArray<TLength extends number, TItem> =
+    number extends TLength ? never :
+    TLength extends number ? IFixedLengthArrayUtil<TLength, TItem, [TItem]> :
+    never
