fbed47c2334a22b46193f63d15164173f7156b2c
Alexandr <a.basalov91@gmail.com>
2025-12-24 12:44:55 +0300

    centroid return type

--- src/algorithm/centroid.ts
+++ src/algorithm/centroid.ts
@@ -1,6 +1,6 @@
 import {type IPoint, Point} from "../figures/index.ts";
 
-export function centroid(...ps: IPoint[]) {
+export function centroid<T extends IPoint>(...ps: T[]): T {
     const sum = Point.sum(...ps);
 
     return Point.scale(sum, 1 / ps.length);
